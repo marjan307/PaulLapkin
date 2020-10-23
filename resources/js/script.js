549ada3bad1d54e893e479a1e7a6a8eb568bd5f0
marjan307 <72312220+marjan307@users.noreply.github.com>
2020-10-23 23:14:46 +0600

    Add files via upload

---
+++ resources/js/script.js
@@ -0,0 +1,31 @@
+//mixitUp
+var mixer = mixitup('.grid-portfolio-container');
+
+//Scrollify
+$(function() {
+    $.scrollify({
+      section : ".scrollify",
+      setHeights: false,
+      updateHash: false,
+    });
+  });
+
+  //Humburger Menu
+
+  function openMenu() {
+      document.getElementById("navbar").style.height = "100%" ;
+  }
+  function closeMenu() {
+    document.getElementById("navbar").style.height = "0%" ;
+}
+
+//Smooth Scroll
+
+$(function(){
+    $('.menu-item a, .scroll-down a').on('click', function(){
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 1000);
+        return false;
+    });
+});
